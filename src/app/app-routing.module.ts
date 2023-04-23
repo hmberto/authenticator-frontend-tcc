57d98013996dada13c9a364aad92a802baab1cae
Humberto <humbertoodantas@gmail.com>
2023-04-22 21:37:48 -0300

    ConfirmAccessComponent added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { SigninGeneratorComponent } from './signin-generator/signin-generator.component';
 import { SigninValidatorComponent } from './signin-validator/signin-validator.component';
 import { FinishSigninComponent } from './finish-signin/finish-signin.component';
+import { ConfirmAccessComponent } from './confirm-access/confirm-access.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signin-generator', component: SigninGeneratorComponent },
   { path: 'signin-validator', component: SigninValidatorComponent },
-  { path: 'finish-signin', component: FinishSigninComponent }
+  { path: 'finish-signin', component: FinishSigninComponent },
+  { path: 'confirm-access', component: ConfirmAccessComponent }
 ];
 
 @NgModule({
